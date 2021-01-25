dea75048498c7a9b8733d4f73a896ff0f4ee57b3
Chen Wang <jeffsan@gmx.com>
2021-01-25 13:59:13 -0500

    update lifelong script

--- lifelong.py
+++ lifelong.py
@@ -14,11 +14,81 @@ from torch.optim import lr_scheduler
 from torch.utils.tensorboard import SummaryWriter
 
 from cnn import CNN
-from fgn import LFGN
+from fgn import FGN
 from ward import WARD
 from train import performance
 from torch_util import count_parameters
 from torch_util import Timer, EarlyStopScheduler
+
+
+class Lifelong(nn.Module):
+    def __init__(self, net, args):
+        super().__init__()
+        self.net, self.args = net, args
+        self.register_buffer('inputs', torch.Tensor(0, 5, 5, 50))
+        self.register_buffer('targets', torch.LongTensor(0))
+        self.sample_viewed = 0
+        self.memory_order = torch.LongTensor()
+        self.criterion = nn.CrossEntropyLoss()
+        self.optimizer = torch.optim.SGD(self.net.parameters(), lr=args.lr, momentum=0.1)
+
+    def observe(self, inputs, targets, reply=True):
+        self.net.train()
+        for i in range(self.args.iteration):
+            self.optimizer.zero_grad()
+            outputs = self.net.forward(inputs)
+            loss = self.criterion(outputs, targets)
+            loss.backward()
+            self.optimizer.step()
+
+        self.sample(inputs, targets)
+        if reply:
+            L = torch.randperm(self.inputs.size(0))
+            minibatches = [L[n:n+self.args.batch_size] for n in range(0, len(L), self.args.batch_size)]
+            for index in minibatches:
+                self.optimizer.zero_grad()
+                inputs, targets = self.inputs[index], self.targets[index]
+                outputs = self.net.forward(inputs)
+                loss = self.criterion(outputs, targets)
+                loss.backward()
+                self.optimizer.step()
+
+    @torch.no_grad()
+    def uniform_sample(self, inputs, targets):
+        self.inputs = torch.cat((self.inputs, inputs), dim=0)
+        self.targets = torch.cat((self.targets, targets), dim=0)
+
+        if self.inputs.size(0) > self.args.memory_size:
+            idx = torch.randperm(self.inputs.size(0))[:self.args.memory_size]
+            self.inputs, self.targets = self.inputs[idx], self.targets[idx]
+
+    @torch.no_grad()
+    def sample(self, inputs, targets):
+        self.sample_viewed += inputs.size(0)
+        self.memory_order += inputs.size(0)# increase the order 
+
+        self.targets = torch.cat((self.targets, targets), dim=0)
+        self.inputs = torch.cat((self.inputs,inputs), dim = 0)
+        self.memory_order = torch.cat((self.memory_order, torch.LongTensor(list(range(inputs.size()[0]-1,-1,-1)))), dim = 0)# for debug
+
+        node_len = int(self.inputs.size(0))
+        ext_memory = node_len - self.args.memory_size
+        if ext_memory > 0:
+            mask = torch.zeros(node_len, dtype = bool) # mask inputs order targets
+            reserve = self.args.memory_size # reserved memrory to be stored
+            seg = np.append(np.arange(0,self.sample_viewed,self.sample_viewed/ext_memory),self.sample_viewed)
+            for i in range(len(seg)-2,-1,-1):
+                left = self.memory_order.ge(np.ceil(seg[i]))*self.memory_order.lt(np.floor(seg[i+1]))
+                leftindex = left.nonzero(as_tuple=False)
+                if leftindex.size()[0] > reserve/(i+1): # the quote is not enough, need to be reduced
+                    leftindex = leftindex[torch.randperm(leftindex.size()[0])[:int(reserve/(i+1))]] # reserve the quote
+                    mask[leftindex] = True
+                else:
+                    mask[leftindex] = True # the quote is enough
+                reserve -= leftindex.size()[0] # deducte the quote
+            self.inputs = self.inputs[mask]
+            self.targets = self.targets[mask]
+            self.memory_order = self.memory_order[mask]
 
 
 
@@ -53,9 +123,7 @@ if __name__ == "__main__":
     train_loader = Data.DataLoader(dataset=train_data, batch_size=args.batch_size, shuffle=False, drop_last=True)
 
     writter = SummaryWriter()
-    net, criterion = LFGN(args).to(args.device), nn.CrossEntropyLoss()
-    optimizer = torch.optim.SGD(net.parameters(), lr=args.lr, momentum=0.1)
-    scheduler = EarlyStopScheduler(optimizer, patience=2, factor=0.1, verbose=True, min_lr=1e-4)
+    net = Lifelong(FGN(), args).to(args.device)
     print('Parameters: %d'%(count_parameters(net)))
     torch.autograd.set_detect_anomaly(True)
     for batch_idx, (inputs, targets) in enumerate(tqdm.tqdm(train_loader)):
