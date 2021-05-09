654223487e99352ada222fd33e38e23f98ff8a45
Yuheng <yuhengq@andrew.cmu.edu>
2021-05-09 20:53:19 +0000

    add data locaation in run scriipt

--- run_all.sh
+++ run_all.sh
@@ -1,19 +1,21 @@
 seed="${1:-0}" 
 device="${2:-cuda:0}"
+data="${3:-/data/datasets}"
 echo "Using seed" $seed
 echo "Using device" $device
+echo "Using dataset location" $data
 
-python nonlifelong.py --seed $seed --device $device --model FGN   --optim SGD 
-python    lifelong.py --seed $seed --device $device --model FGN   --optim SGD
+python nonlifelong.py --seed $seed --device $device --data-root $data --model FGN   --optim SGD
+python    lifelong.py --seed $seed --device $device --data-root $data --model FGN   --optim SGD
 
-python nonlifelong.py --seed $seed --device $device --model GAT   --optim Adam
-python    lifelong.py --seed $seed --device $device --model GAT   --optim Adam
+python nonlifelong.py --seed $seed --device $device --data-root $data --model GAT   --optim Adam
+python    lifelong.py --seed $seed --device $device --data-root $data --model GAT   --optim Adam
 
-python nonlifelong.py --seed $seed --device $device --model AFGN  --optim Adam
-python    lifelong.py --seed $seed --device $device --model AFGN  --optim Adam
+python nonlifelong.py --seed $seed --device $device --data-root $data --model AFGN  --optim Adam
+python    lifelong.py --seed $seed --device $device --data-root $data --model AFGN  --optim Adam
 
-python nonlifelong.py --seed $seed --device $device --model GCN   --optim SGD
-python    lifelong.py --seed $seed --device $device --model GCN   --optim SGD
+python nonlifelong.py --seed $seed --device $device --data-root $data --model GCN   --optim SGD
+python    lifelong.py --seed $seed --device $device --data-root $data --model GCN   --optim SGD
 
-python nonlifelong.py --seed $seed --device $device --model APPNP --optim SGD 
-python    lifelong.py --seed $seed --device $device --model APPNP --optim SGD
+python nonlifelong.py --seed $seed --device $device --data-root $data --model APPNP --optim SGD
+python    lifelong.py --seed $seed --device $device --data-root $data --model APPNP --optim SGD
