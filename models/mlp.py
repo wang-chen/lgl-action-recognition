13b937f540d631f2d30cb913d5ce65036594f2d4
Chen Wang <jeffsan@gmx.com>
2021-05-16 12:50:23 -0400

    update mlp model

--- models/mlp.py
+++ models/mlp.py
@@ -7,8 +7,8 @@ import torch.nn as nn
 class MLP(nn.Module):
     def __init__(self):
         super().__init__()
-        self.feat1 = nn.Sequential(nn.Flatten(), nn.Linear(50*5*5, 11*5*5), nn.ReLU())
-        self.feat2 = nn.Sequential(nn.Linear(11*5*5, 32*12), nn.ReLU())
+        self.feat1 = nn.Sequential(nn.Flatten(), nn.Linear(50*5*5, 32*5*5), nn.ReLU())
+        self.feat2 = nn.Sequential(nn.Linear(32*5*5, 32*12), nn.ReLU())
         self.linear = nn.Sequential(nn.Linear(32*12, 13))
 
     def forward(self, x):
